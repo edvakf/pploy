dddcd7133c247d30e6396c9a52344dee9db23721
Atsushi Takayama <taka.atsushi@gmail.com>
2014-08-28 18:01:33 +0900

    Add global error handling

--- app/Global.scala
+++ app/Global.scala
@@ -1,10 +1,14 @@
+import java.net.URL
+
 import play.api._
 import play.api.Play.current
 import play.api._
 import play.api.mvc._
 import play.filters.csrf._
+import play.api.mvc.Results._
+import scala.concurrent.Future
 
-import models.{Project, WorkingDir}
+import models._
 
 object Global extends WithFilters(CSRFFilter()) with GlobalSettings {
 
@@ -13,6 +17,33 @@ object Global extends WithFilters(CSRFFilter()) with GlobalSettings {
       case None => throw new RuntimeException("pploy.dir is not set")
       case Some(dir) => WorkingDir.setup(dir)
     }
+  }
+
+  private def backUrl(request: RequestHeader) = {
+    request.headers.get("referer") match {
+      case Some(url) => new URL(url).getFile
+      case None => "/"
+    }
+  }
+
+  override def onError(request: RequestHeader, ex: Throwable) = {
+    Future.successful(
+      Redirect(backUrl(request))
+        .flashing("message" -> ex.getMessage)
+    )
+  }
+
+  override def onHandlerNotFound(request: RequestHeader) = {
+    Future.successful(
+      Redirect(backUrl(request))
+    )
+  }
+
+  override def onBadRequest(request: RequestHeader, error: String) = {
+    Future.successful(
+      Redirect(backUrl(request))
+        .flashing("message" -> error)
+    )
   }
 
 }
