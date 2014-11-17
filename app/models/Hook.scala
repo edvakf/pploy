474e1e661428e2be3514dfd47d415fd7fd3c0ffe
toshi <toshi@pixiv.com>
2014-11-17 19:10:42 +0900

    Use `foreach` instead of `map` for side effects

--- app/models/Hook.scala
+++ app/models/Hook.scala
@@ -10,19 +10,19 @@ object Hook {
   implicit val lang = Lang.availables.head
 
   def lockGained(projectName: String, userName: String) = {
-    idobata.map { _.postHTML(Messages("hook.lock.gained", projectName, userName)) }
+    idobata.foreach { _.postHTML(Messages("hook.lock.gained", projectName, userName)) }
   }
 
   def lockReleased(projectName: String, userName: String) = {
-    idobata.map { _.postHTML(Messages("hook.lock.released", projectName, userName)) }
+    idobata.foreach { _.postHTML(Messages("hook.lock.released", projectName, userName)) }
   }
 
   def lockExtended(projectName: String, userName: String) = {
-    idobata.map { _.postHTML(Messages("hook.lock.extended", projectName, userName)) }
+    idobata.foreach { _.postHTML(Messages("hook.lock.extended", projectName, userName)) }
   }
 
   def deployed(projectName: String, userName: String) = {
-    idobata.map { _.postHTML(Messages("hook.deployed", projectName, userName)) }
+    idobata.foreach { _.postHTML(Messages("hook.deployed", projectName, userName)) }
   }
 
 }
