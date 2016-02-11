68483859475a5c01f852b7996571b8fa8774a981
Atsushi Takayama <taka.atsushi@gmail.com>
2016-02-11 10:33:25 +0900

    Refactor ProcessEnumerator

--- app/models/ProcessEnumerator.scala
+++ app/models/ProcessEnumerator.scala
@@ -1,9 +1,7 @@
 package models
 
-import java.io.{ BufferedReader, InputStreamReader, PipedOutputStream, PipedInputStream }
 import play.api.libs.iteratee.Enumerator
 import scala.sys.process._
-import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object ProcessEnumerator {
@@ -11,21 +9,8 @@ object ProcessEnumerator {
   // and executes the process in Future
   // so that the process' output can be streamed
   def apply(process: ProcessBuilder): Enumerator[String] = {
-    val in = new PipedInputStream()
-    val out = new PipedOutputStream(in)
-    Future(process.#>(out).run())
-
-    val reader = new BufferedReader(new InputStreamReader(in, "UTF-8"))
-
-    // there is Enumerator.fromString which makes Enumerator[Array[Byte]]
-    // but we prefer Enumerator[String] so that WebSocket uses TextFrame
-    Enumerator.fromCallback1[String](_ => Future {
-      reader.readLine match {
-        case line: String => Some(line + "\n")
-        case _ => None
-      }
-    }, { () => // oncomplete
-      in.close()
-    })
+    Enumerator.enumerate[String](
+      process.lineStream_!(ProcessLogger(line => ())).map { line => line + "\n" }
+    )
   }
 }
