444a9557303b2e55809036468a5a48cf389aa50a
Atsushi Takayama <taka.atsushi@gmail.com>
2016-02-11 18:01:11 +0900

    Support Slack webhook (fixes #9)

--- app/models/Hook.scala
+++ app/models/Hook.scala
@@ -2,27 +2,51 @@ package models
 
 import play.api.Play._
 import play.api.i18n.{ Lang, Messages }
+import play.api.libs.ws.WS
+import play.api.libs.json._
 
 object Hook {
   val idobata = current.configuration.getString("pploy.idobata.endpoint").map {
-    url => IdobataGenericHook(url)
+    url => new IdobataGenericHook(url)
+  }
+  val slack = current.configuration.getString("pploy.slack.endpoint").map {
+    url => new SlackIncomingWebHook(url)
   }
   implicit val lang = Lang.availables.head
 
   def lockGained(projectName: String, userName: String) = {
-    idobata.foreach { _.postHTML(Messages("hook.lock.gained", projectName, userName)) }
+    idobata.foreach { _.post(Messages("hook.lock.gained", projectName, userName)) }
+    slack.foreach { _.post(Messages("hook.lock.gained", projectName, userName)) }
   }
 
   def lockReleased(projectName: String, userName: String) = {
-    idobata.foreach { _.postHTML(Messages("hook.lock.released", projectName, userName)) }
+    idobata.foreach { _.post(Messages("hook.lock.released", projectName, userName)) }
+    slack.foreach { _.post(Messages("hook.lock.released", projectName, userName)) }
   }
 
   def lockExtended(projectName: String, userName: String) = {
-    idobata.foreach { _.postHTML(Messages("hook.lock.extended", projectName, userName)) }
+    idobata.foreach { _.post(Messages("hook.lock.extended", projectName, userName)) }
+    slack.foreach { _.post(Messages("hook.lock.extended", projectName, userName)) }
   }
 
   def deployed(projectName: String, userName: String, target: String) = {
-    idobata.foreach { _.postHTML(Messages("hook.deployed", projectName, userName, target)) }
+    idobata.foreach { _.post(Messages("hook.deployed", projectName, userName, target)) }
+    slack.foreach { _.post(Messages("hook.deployed", projectName, userName)) }
   }
 
+}
+
+class IdobataGenericHook(endpoint: String) {
+  def post(html: String): Unit = {
+    WS.url(endpoint).post(Map("format" -> Seq("html"), "source" -> Seq(html)))
+  }
+}
+
+class SlackIncomingWebHook(endpoint: String) {
+  def post(text: String): Unit = {
+    val payload = Json.obj(
+      "text" -> text
+    )
+    WS.url(endpoint).post(payload)
+  }
 }
