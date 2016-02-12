1c3d7c171c507fbd061635bdf980ab29398f31c7
Atsushi Takayama <taka.atsushi@gmail.com>
2016-02-12 20:43:48 +0900

    Support git 1.7 which doesn't have %cI date format

--- app/models/Commit.scala
+++ app/models/Commit.scala
@@ -16,20 +16,21 @@ object Commit {
     "--name-status", // show list of file diffs
     "-m", // show file diffs for merge commit
     "--first-parent", // -m shows file diffs from each parent. --first-parent make it from the first parent
-    s"--pretty=format:${delim1}%H${delim2}%cI${delim2}%an${delim2}%D${delim2}%s${delim2}%b${delim2}"
+    s"--pretty=format:${delim1}%H${delim2}%ci${delim2}%an${delim2}%D${delim2}%s${delim2}%b${delim2}"
   )
 
   def parse(logs: String): Seq[Commit] = {
     logs.split(delim1).drop(1).map { log =>
       log.split(delim2).toSeq match {
-        case Seq(hash, isoDate, author, refs, subject, body, nameStatus) =>
+        case Seq(hash, isoLikeDate, author, refs, subject, body, nameStatus) =>
           val otherRefs = refs.split(", ")
             .flatMap(_.split(" -> ")) // master becomes "HEAD -> refs/head/master"
             .filterNot(_ == "") // filter out the case when refs is an empty string
 
           Commit(
             hash,
-            ISODateTimeFormat.dateTimeNoMillis.parseDateTime(isoDate),
+            // newer version of git has `%cI` format which can be parsed with `ISODateTimeFormat.dateTimeNoMillis`, but git 1.7 doesn't...
+            DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss Z").parseDateTime(isoLikeDate),
             author,
             otherRefs,
             subject,
