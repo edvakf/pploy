b97fb5bdc8aacab245a2e88cf070601939850bf2
Atsushi Takayama <taka.atsushi@gmail.com>
2016-02-12 21:52:12 +0900

    Support git 1.7 (use %d instead of %D)

--- app/models/Commit.scala
+++ app/models/Commit.scala
@@ -16,16 +16,22 @@ object Commit {
     "--name-status", // show list of file diffs
     "-m", // show file diffs for merge commit
     "--first-parent", // -m shows file diffs from each parent. --first-parent make it from the first parent
-    s"--pretty=format:${delim1}%H${delim2}%ci${delim2}%an${delim2}%D${delim2}%s${delim2}%b${delim2}"
+    s"--pretty=format:${delim1}%H${delim2}%ci${delim2}%an${delim2}%d${delim2}%s${delim2}%b${delim2}"
   )
 
   def parse(logs: String): Seq[Commit] = {
     logs.split(delim1).drop(1).map { log =>
       log.split(delim2).toSeq match {
         case Seq(hash, isoLikeDate, author, refs, subject, body, nameStatus) =>
-          val otherRefs = refs.split(", ")
-            .flatMap(_.split(" -> ")) // master becomes "HEAD -> refs/head/master"
-            .filterNot(_ == "") // filter out the case when refs is an empty string
+          val re = """^ \((.*?)\)$""".r // git 1.7 doesn't have `%D` format, which is the same as `%d` without the parens
+          val otherRefs: Seq[String] = refs match {
+            case re(foo) =>
+              foo.split(", ")
+                .flatMap(_.split(" -> ")) // master becomes "HEAD -> refs/head/master"
+                .filterNot(_ == "") // filter out the case when refs is an empty string
+            case _ =>
+              Seq()
+          }
 
           Commit(
             hash,
